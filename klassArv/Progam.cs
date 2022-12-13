f77bc028c6e898fbf2aaad4aebb4f87e7980a45d
Vincent <vinelb050125@student.jenseneducation.se>
2022-12-13 13:40:00 +0100

    Add project files.

---
+++ klassArv/Progam.cs
@@ -0,0 +1,13 @@
+﻿namespace klassArv
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            fordon newfordon = new fordon(4, "el");
+            fordon newfordon1 = new fordon(4, "bensin");
+        }
+    }
+}
+
+   
---
+++ klassArv/fordon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klassArv
+{
+    internal class fordon
+    {
+        public int däck { get; set; }
+        public string drivmedel { get; set; }
+
+        public fordon(int däck, string drivmedel)
+        {
+            this.däck = däck;
+            this.drivmedel = drivmedel;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
